2c48d831260d8065257f74121c8da1586932560f
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-11-01 00:25:53 +0300

    использовал полноту api github и получил всю информацию из одного запроса

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,19 @@
+import requests
+import datetime
 
 
 def get_trending_repositories(top_size):
-    pass
+    r = requests.get(
+        'https://api.github.com/search/repositories?q=created:>'+get_monday_date()+'&sort=stars')
+    for item in r.json()['items'][:top_size]:
+        print ('The repository "{}":\nLink:"{}"\nStars: {}\nOpen issues {}\n'.format(
+            item['name'], item['html_url'], item['stargazers_count'], item['open_issues_count']))
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_monday_date():
+    return str((datetime.date.today() - datetime.timedelta(weeks=1)))
+
 
 if __name__ == '__main__':
-    pass
+    get_monday_date()
+    get_trending_repositories(20)
