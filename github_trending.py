267b63abaca6309210d6b7dfc982cdf5358a044e
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-11-01 21:26:51 +0300

    сделал вывод отдельно, исправил опечатки

--- github_trending.py
+++ github_trending.py
@@ -5,15 +5,18 @@ import datetime
 def get_trending_repositories(top_size):
     r = requests.get(
         'https://api.github.com/search/repositories?q=created:>'+get_monday_date()+'&sort=stars')
-    for item in r.json()['items'][:top_size]:
-        print ('The repository "{}":\nLink:"{}"\nStars: {}\nOpen issues {}\n'.format(
-            item['name'], item['html_url'], item['stargazers_count'], item['open_issues_count']))
+    return r.json()['items'][:top_size]
 
 
 def get_monday_date():
     return str((datetime.date.today() - datetime.timedelta(weeks=1)))
 
 
+def pprint_repo_list(repositories):
+    for repo in repositories:
+        print ('The repository "{}":\nLink:"{}"\nStars: {}\nOpen issues {}\n'.format(
+            repo['name'], repo['html_url'], repo['stargazers_count'], repo['open_issues_count']))
+
+
 if __name__ == '__main__':
-    get_monday_date()
-    get_trending_repositories(20)
+    pprint_repo_list(get_trending_repositories(20))
