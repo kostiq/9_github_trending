fd7ad052204645516a52b5ed31c247c8faf93dd8
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-11-22 13:31:32 +0300

    объединил функции, сделал корректно get запрос, использовал format

--- github_trending.py
+++ github_trending.py
@@ -3,19 +3,20 @@ import datetime
 
 
 def get_trending_repositories(top_size):
+    last_week_date = str(datetime.date.today() - datetime.timedelta(weeks=1))
+    payload = {'q': 'created:>'+last_week_date, 'sort': 'stars'}
     r = requests.get(
-        'https://api.github.com/search/repositories?q=created:>'+get_monday_date()+'&sort=stars')
+        'https://api.github.com/search/repositories', params=payload)
     return r.json()['items'][:top_size]
-
-
-def get_monday_date():
-    return str((datetime.date.today() - datetime.timedelta(weeks=1)))
 
 
 def pprint_repo_list(repositories):
     for repo in repositories:
-        print ('The repository "{}":\nLink:"{}"\nStars: {}\nOpen issues {}\n'.format(
-            repo['name'], repo['html_url'], repo['stargazers_count'], repo['open_issues_count']))
+        print ('''The repository {name!r}:
+            Link:{html_url!r}
+            Stars: {stargazers_count}
+            Open issues: {open_issues_count}'''
+               .format(**repo))
 
 
 if __name__ == '__main__':
